6f743ed989b0de9bfeeaf63f9632c15f98050b26
AmanPrasad-Simform <aman.p@simformsolutions.com>
2023-04-18 20:06:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+Vue.createApp({
+    data() {
+        return {
+            count: 0,
+            message: "",
+            inputValue:"", 
+            firstName: "Aman",
+            lastName: "Prasad",
+            role: "",
+            age:21,
+            url:"https://www.google.com/" ,
+            isRed: true,
+            content: '<span>V-html displaying that posses security risk bcoz link can be changed</span><a href="https://www.google.com/" target="_blank">Link</a>',
+        }
+    },
+    methods: {
+        increment(){
+            this.age++;
+        },
+        name() {
+            return `${this.firstName} ${this.lastName}`
+        },
+        lastname(msg,event){
+            this.lastName = event.target.value
+        }
+    },
+    computed: {
+        calc(){
+            return 2
+        }
+    },
+    watch: {
+        inputValue(newValue, oldValue) {
+          this.message = 'Input value changed to: ' + newValue
+        }
+      }
+}).mount('#app')

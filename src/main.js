69dfb75f0e399a4c82de08f084a5b3d8a5e86f9c
AmanPrasad-Simform <aman.p@simformsolutions.com>
2023-04-26 15:50:20 +0530

    Setup of the bootstrap

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import '../src/assets/scss/style.scss'
 import Navbar from './components/Navbar.vue'
+// Import our custom CSS
+import './assets/scss/style.scss'
+
+// Import all of Bootstrap's JS
+import {Modal} from 'bootstrap'
 
 let vm = createApp(App)
 

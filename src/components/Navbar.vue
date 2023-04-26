ae8f58f43e441c77eef52090dde8a9b11ad992bc
AmanPrasad-Simform <aman.p@simformsolutions.com>
2023-04-26 15:14:23 +0530

    Added bootstrap

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <div class="navbar">
     <div class="navbar-logo">
     <h1>CarNation</h1>
-    <img src="../../public/favicon-car.png">
+    <img src="/favicon-car.png">
     </div>
     <p>- Accelerate your journey with us.</p>
   </div>

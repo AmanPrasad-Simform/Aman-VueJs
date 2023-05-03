aeb650a98463596f6c28eb1a1ccadda701822114
AmanPrasad-Simform <aman.p@simformsolutions.com>
2023-05-03 12:59:49 +0530

    refactored: Improved components structure and enhanced performance

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
-<Navbar/>
-<GalleryCard @car-price='showPrice' />
+  <Navbar />
+  <GalleryCard @car-price="showPrice" />
 </template>
 
 <script>
-import GalleryCard from './components/GalleryCard.vue';
-import './assests/GalleryCard.css';
+import GalleryCard from "./components/GalleryCard.vue";
+import "./assets/style.css";
 
 export default {
-  components: {GalleryCard },
-  methods:{
-    showPrice(car){
-      swal(car.carName,car.carPrice)
-        }
-  }
-}
+  components: { GalleryCard },
+  methods: {
+    showPrice(car) {
+      swal(car.carName, car.carPrice);
+    },
+  },
+};
 </script>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,15 +1,73 @@
 <template>
   <div class="navbar">
     <div class="navbar-logo">
-    <h1>CarNation</h1>
-    <img src="/favicon-car.png">
+      <h1>{{ carShowRoomName }}</h1>
+      <img src="/favicon-car.png" />
     </div>
-    <p>- Accelerate your journey with us.</p>
+    <p>{{ tagLine }}</p>
   </div>
 </template>
 
 <script>
-export default{
-    name: 'Navbar'
-}
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      carShowRoomName: "CarNation",
+      tagLine: "Accelerate your journey with us.",
+    };
+  },
+};
 </script>
+
+<style scoped>
+.navbar {
+  background-color: #39484a;
+  overflow: hidden;
+  width: 100%;
+  position: sticky;
+  top: 0;
+  text-align: center;
+  z-index: 10;
+}
+.navbar-logo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.navbar-logo img {
+  width: 65px;
+}
+.navbar h1 {
+  line-height: 1;
+  padding: 0.5rem;
+  font-weight: 600;
+  font-size: 35px;
+  background: #f1f6f9;
+  background-size: cover;
+  -webkit-background-clip: text;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+.navbar p {
+  line-height: 1;
+  margin: 0;
+  padding: 0.5rem;
+  font-weight: 600;
+  background: #f1f6f9;
+  font-size: 20px;
+  background-size: cover;
+  -webkit-background-clip: text;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+@media (max-width: 768px) {
+  .navbar h1,
+  .navbar p {
+    display: block;
+    text-align: center;
+  }
+}
+</style>
